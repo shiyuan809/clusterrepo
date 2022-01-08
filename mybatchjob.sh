8d13e9680f973a5e8db84baf0f0747d28f099f21
Yuan Shi <38163562+shiyuan809@users.noreply.github.com>
2022-01-07 19:46:21 -0500

    Update mybatchjob.sh

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+#SBATCH -n 1 #Request 4 tasks
+#SBATCH -N 1 #Request 1 note
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
-#SBATCH --partition=sched_mit_sloan_batch
+#SBATCH -p sched_mit_sloan_batch
 #SBATCH --time=0-00:10
 #SBATCH -o /home/yuansh/clusterrepo/myoutputfile.out
 #SBATCH -e /home/yuansh/clusterrepo/myerrorfile.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=yuansh@mit.edu
 
-module load julia/0.6.1
+module load julia/1.5.2
 
 julia myscript.jl
