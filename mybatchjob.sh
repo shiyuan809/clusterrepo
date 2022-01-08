2ca309a8a5ba29d8df55e855d53963e7c725cc5a
Yuan Shi <38163562+shiyuan809@users.noreply.github.com>
2022-01-07 19:56:40 -0500

    Update mybatchjob.sh

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -2,10 +2,10 @@
 #SBATCH -n 4 #Request 4 tasks (cores)
 #SBATCH -N 1 #Request 1 node
 #SBATCH -t 0-00:30 #Request runtime of 30 minutes
-#SBATCH -p sched_mit_sloan_batch #Run on sched_engaging_default partition
+#SBATCH -p sched_mit_sloan_interactive #Run on sched_engaging_default partition
 #SBATCH --mem-per-cpu=4000 #Request 4G of memory per CPU
-#SBATCH -o output_%j.txt #redirect output to output_JOBID.txt
-#SBATCH -e error_%j.txt #redirect errors to error_JOBID.txt
+#SBATCH -o /home/yuansh/clusterrepo/myoutputfile.out #redirect output to output_JOBID.txt
+#SBATCH -e /home/yuansh/clusterrepo/myerrorfile.err #redirect errors to error_JOBID.txt
 #SBATCH --mail-type=BEGIN,END #Mail when job starts and ends
 #SBATCH --mail-user=yuansh@mit.edu #email recipient
 
