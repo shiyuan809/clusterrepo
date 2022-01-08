603a8180f32199e106ceb598953b3d2a03c82ac5
Yuan Shi <38163562+shiyuan809@users.noreply.github.com>
2022-01-07 20:01:48 -0500

    Final update to .sh file

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -1,13 +1,12 @@
-#!/bin/bash 
-#SBATCH -n 4 #Request 4 tasks (cores)
-#SBATCH -N 1 #Request 1 node
-#SBATCH -t 0-00:30 #Request runtime of 30 minutes
-#SBATCH -p sched_mit_sloan_interactive #Run on sched_engaging_default partition
-#SBATCH --mem-per-cpu=4000 #Request 4G of memory per CPU
-#SBATCH -o /home/yuansh/clusterrepo/myoutputfile.out #redirect output to output_JOBID.txt
-#SBATCH -e /home/yuansh/clusterrepo/myerrorfile.err #redirect errors to error_JOBID.txt
-#SBATCH --mail-type=BEGIN,END #Mail when job starts and ends
-#SBATCH --mail-user=yuansh@mit.edu #email recipient
+#!/bin/bash
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --partition=sched_mit_sloan_interactive
+#SBATCH --time=0-00:10
+#SBATCH -o /home/yuansh/clusterrepo/myoutputfile.out
+#SBATCH -e /home/yuansh/clusterrepo/myerrorfile.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=yuansh@mit.edu
 
 module load julia/1.5.2
 
